589b49068a720de4cbc6947375ee2554935e0239
nitin-7477 <gautam.nitinji@gmail.com>
2023-12-25 12:06:27 +0530

    Applicaton Not Loading

--- App.js
+++ App.js
@@ -53,33 +53,33 @@ export default function App() {
         {/* {isFirstLaunch && (
           <Stack.Screen options={{ headerShown: false }} name="OnboardingScreen" component={OnboardingScreen} />
         )} */}
-        <Stack.Screen options={{ headerShown: false }} name="Register" component={Register} />
+        {/* <Stack.Screen options={{ headerShown: false }} name="Register" component={Register} /> */}
         <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
-        <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
-        <Stack.Screen options={{ headerShown: false }} name="Gamescreen" component={Gamescreen} />
-        <Stack.Screen name="Wallet" options={{ headerShown: false }} component={Wallet} />
-        <Stack.Screen options={{ headerShown: false }} name="ForgotPassword" component={ForgotPasswordComponent} />
+        {/* <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} /> */}
+        {/* <Stack.Screen options={{ headerShown: false }} name="Gamescreen" component={Gamescreen} /> */}
+        {/* <Stack.Screen name="Wallet" options={{ headerShown: false }} component={Wallet} /> */}
+        {/* <Stack.Screen options={{ headerShown: false }} name="ForgotPassword" component={ForgotPasswordComponent} /> */}
 
-        <Stack.Screen options={{ headerShown: false }} name="Notification" component={Notifications} />
-        <Stack.Screen name="FeedbackForm" options={{ headerShown: false }} component={FeedbackForm} />
-        <Stack.Screen name="Account" options={{ headerShown: false }} component={Account} />
-        <Stack.Screen name="Setting" options={{ headerShown: false }} component={Setting} />
-        <Stack.Screen name="Gifts" options={{ headerShown: false }} component={Gifts} />
-        <Stack.Screen name="GameStats" options={{ headerShown: false }} component={GameStats} />
-        <Stack.Screen name="Language" options={{ headerShown: false }} component={Language} />
-        <Stack.Screen name="SettingLogin" options={{ headerShown: false }} component={SettingLogin} />
-        <Stack.Screen name="BindMail" options={{ headerShown: false }} component={BindMail} />
+        {/* <Stack.Screen options={{ headerShown: false }} name="Notification" component={Notifications} /> */}
+        {/* <Stack.Screen name="FeedbackForm" options={{ headerShown: false }} component={FeedbackForm} /> */}
+        {/* <Stack.Screen name="Account" options={{ headerShown: false }} component={Account} /> */}
+        {/* <Stack.Screen name="Setting" options={{ headerShown: false }} component={Setting} /> */}
+        {/* <Stack.Screen name="Gifts" options={{ headerShown: false }} component={Gifts} /> */}
+        {/* <Stack.Screen name="GameStats" options={{ headerShown: false }} component={GameStats} /> */}
+        {/* <Stack.Screen name="Language" options={{ headerShown: false }} component={Language} /> */}
+        {/* <Stack.Screen name="SettingLogin" options={{ headerShown: false }} component={SettingLogin} /> */}
+        {/* <Stack.Screen name="BindMail" options={{ headerShown: false }} component={BindMail} /> */}
 
-        <Stack.Screen name="TimerComponent" options={{ headerShown: false }} component={TimerComponent} />
-        <Stack.Screen name="TimerWithModal30Sec" options={{ headerShown: false }} component={TimerWithModal30Sec} />
-        <Stack.Screen name="TimerWithModal60Sec" options={{ headerShown: false }} component={TimerWithModal60Sec} />
-        <Stack.Screen name="TimerWithModal3Min" options={{ headerShown: false }} component={TimerWithModal3Min} />
-        <Stack.Screen name="TimerWithModal5Min" options={{ headerShown: false }} component={TimerWithModal5Min} />
+        {/* <Stack.Screen name="TimerComponent" options={{ headerShown: false }} component={TimerComponent} /> */}
+        {/* <Stack.Screen name="TimerWithModal30Sec" options={{ headerShown: false }} component={TimerWithModal30Sec} /> */}
+        {/* <Stack.Screen name="TimerWithModal60Sec" options={{ headerShown: false }} component={TimerWithModal60Sec} /> */}
+        {/* <Stack.Screen name="TimerWithModal3Min" options={{ headerShown: false }} component={TimerWithModal3Min} /> */}
+        {/* <Stack.Screen name="TimerWithModal5Min" options={{ headerShown: false }} component={TimerWithModal5Min} /> */}
 
-        <Stack.Screen name="DepositeScreen" options={{ headerShown: false }} component={DepositeScreen} />
-        <Stack.Screen name="WithdrawScreen" options={{ headerShown: false }} component={WithdrawScreen} />
-        <Stack.Screen name="DepositHistoryScreen" options={{ headerShown: false }} component={DepositHistoryScreen} />
-        <Stack.Screen name="WithdrawHistoryScreen" options={{ headerShown: false }} component={WithdrawHistoryScreen} />
+        {/* <Stack.Screen name="DepositeScreen" options={{ headerShown: false }} component={DepositeScreen} /> */}
+        {/* <Stack.Screen name="WithdrawScreen" options={{ headerShown: false }} component={WithdrawScreen} /> */}
+        {/* <Stack.Screen name="DepositHistoryScreen" options={{ headerShown: false }} component={DepositHistoryScreen} /> */}
+        {/* <Stack.Screen name="WithdrawHistoryScreen" options={{ headerShown: false }} component={WithdrawHistoryScreen} /> */}
 
 
 
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 /**
  * @format
  */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './App'; // Your main component
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
 
